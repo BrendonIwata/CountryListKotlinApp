02460528ba3beba47f223fc19c3e45dcf37ced00
Brendon Iwata <brendon_gi.13@hotmail.com>
2020-11-29 21:24:39 -0400

    implementação de activity para inserir foto igual a do whatsapp

--- app/src/main/java/br/com/iwatasoftware/basickotlinapp/activity/cropimage/CropImageActivity.kt
+++ app/src/main/java/br/com/iwatasoftware/basickotlinapp/activity/cropimage/CropImageActivity.kt
@@ -1,38 +1,55 @@
 package br.com.iwatasoftware.basickotlinapp.activity.cropimage
 
 import android.graphics.BitmapFactory
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.util.DisplayMetrics
-import android.widget.Button
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import br.com.iwatasoftware.basickotlinapp.R
+import com.google.android.material.snackbar.Snackbar
 import com.theartofdev.edmodo.cropper.CropImageView
 import kotlinx.android.synthetic.main.activity_crop_image.*
 
 class CropImageActivity : AppCompatActivity() {
 
-    private var isCroped = false
-    private lateinit var crop : CropImageView
-    private lateinit var btnContinuar : Button
-    private lateinit var btnCancelar : Button
+    private lateinit var crop: CropImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_crop_image)
 
-        crop = img_crop
-        btnContinuar = btn_done
-        btnCancelar = btn_cancel
+        supportActionBar?.setTitle(R.string.recorte_foto)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setBackgroundDrawable(ColorDrawable(Color.BLACK))
 
-        var displayMetrics = DisplayMetrics()
+        crop = img_crop
+
+        val displayMetrics = DisplayMetrics()
         windowManager.defaultDisplay.getMetrics(displayMetrics)
-        var width = displayMetrics.widthPixels
-        width = width - 80
+        val width = displayMetrics.widthPixels
         img_crop.layoutParams.height = width
         img_crop.layoutParams.width = width
         img_crop.background = null
         img_crop.scaleType = CropImageView.ScaleType.FIT_CENTER
-        var icon = BitmapFactory.decodeResource(resources, R.drawable.teste)
+        val icon = BitmapFactory.decodeResource(resources, R.drawable.teste)
         img_crop.setImageBitmap(icon)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_cropped_image, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_crop_image_save -> {
+                Snackbar.make(crop.rootView, "teste", Snackbar.LENGTH_LONG).show()
+            }
+        }
+        return true
     }
 }
